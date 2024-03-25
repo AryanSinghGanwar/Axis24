c4cc7fa6f272fc4adbbd11f8b91bec82dd9259c1
Aryan Singh <aryansingh81104@gmail.com>
2024-03-25 14:33:15 +0530

    Initial commit

---
+++ Assets/main.js
@@ -0,0 +1,27 @@
+
+function showsidebar(){
+    const sidebar=document.querySelector('.sidebar')
+    sidebar.style.display='flex'
+}
+
+function hidesid(){
+    const sidebar=document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+function ha(){
+    const sidebar=document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
